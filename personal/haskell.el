09d9cf1f1faa0652b871e04f2952c8007912ffb7
Eric Rochester <erochest@virginia.edu>
2015-10-16 06:16:28 -0400

    Theme change and playing with Haskell config.

--- personal/haskell.el
+++ personal/haskell.el
@@ -1,7 +1,14 @@
 (require 'haskell-mode)
 (require 'haskell-cabal)
+; (require 'stack-mode)
+; (require 'flycheck-haskell)
 
 (setq haskell-stylish-on-save t)
 
 (add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
 (add-hook 'haskell-mode-hook 'interactive-haskell-mode)
+; (eval-after-load 'flycheck
+;   '(add-hook 'flycheck-mode-hook #'flycheck-haskell-setup))
+
+; (add-hook 'haskell-mode-hook 'stack-mode)
+; (add-to-list 'company-backends 'company-ghc)
