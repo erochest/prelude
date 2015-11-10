368f9ad19207dddf9c29ebad06fb894e280d127e
Eric Rochester <erochest@virginia.edu>
2015-11-10 06:43:24 -0500

    Updated Haskell config.
    https://www.fpcomplete.com/user/commercial/content/ide-and-linters

--- personal/haskell.el
+++ personal/haskell.el
@@ -2,19 +2,37 @@
 (require 'haskell-cabal)
 (require 'flycheck-haskell)
 
+;;;; Set-up from https://www.fpcomplete.com/user/commercial/content/ide-and-linters
+
+(defun turn-on-subword-mode ()
+  (interactive)
+  (subword-mode 1))
+
+(defun my-haskell-mode-hook ()
+  (ghc-init)
+  (define-key haskell-mode-map (kbd "C-c C-l") 'haskell-process-load-or-reload)
+  (define-key haskell-mode-map (kbd "C-`") 'haskell-interactive-bring)
+  (define-key haskell-mode-map (kbd "C-c C-t") 'haskell-process-do-type)
+  (define-key haskell-mode-map (kbd "C-c C-i") 'haskell-process-do-info)
+  (define-key haskell-mode-map (kbd "C-c C-k") 'haskell-interactive-mode-clear)
+  (define-key haskell-mode-map (kbd "SPC") 'haskell-mode-contextual-space))
+
+(custom-set-variables
+ '(haskell-mode-hook
+   (quote
+    (interactive-haskell-mode
+     turn-on-haskell-indent
+     turn-on-subword-mode
+     turn-on-haskell-decl-scan
+     my-haskell-mode-hook)))
+ '(haskell-process-type 'stack-ghci))
+
 (setq haskell-stylish-on-save t)
 
-
-(add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
-(add-hook 'haskell-mode-hook 'interactive-haskell-mode)
 (eval-after-load 'flycheck
   '(add-hook 'flycheck-mode-hook #'flycheck-haskell-setup))
 
-; (add-to-list 'company-backends 'company-ghc)
+(autoload 'ghc-init "ghc" nil t)
+(autoload 'ghc-debug "ghc" nil t)
 
-;; for structured-haskell-mode
-; (require 'shm)
-; (add-hook 'haskell-mode-hook 'structured-haskell-mode)
-
-; (set-face-background 'shm-current-face "#eee8d5")
-; (set-face-background 'shm-quarantine-face "lemonchiffon")
+(add-to-list 'company-backends 'company-ghc)
