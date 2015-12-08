c1baf9c70e16964d7f2ec0d062ac442c088a69f8
Eric Rochester <erochest@virginia.edu>
2015-12-08 16:56:50 -0500

    Org mode settings.

--- personal/org.el
+++ personal/org.el
@@ -1,6 +1,14 @@
 (setq org-capture-templates
-      '(("j" "Journal entry" plain
+      '(("j" "Journal entry" entry
          (file+datetree+prompt "~/Dropbox/notes/journal.org")
-         "%K - %a\n%i\n%?\n")))
+         "* %T\n%?\n%K - %a\n%i\n")
+        ("t" "Time log" plain
+         (file+datetree+prompt "~/Dropbox/notes/timelog.org")
+
+         )
+        ))
 
 (global-set-key (kbd "C-c C-j") 'org-capture)
+
+(mapc 'load '("org-sync" "org-sync-github"))
+(require 'org-trello)
